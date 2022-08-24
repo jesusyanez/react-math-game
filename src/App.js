cfb20d9507aeb8067e98c2fa7eaab48880372b44
jesusyanez <96055925+jesusyanez@users.noreply.github.com>
2022-08-24 04:04:07 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [answers, setAnswers] = useState({ main: 0, num1: 0, num2: 0, choices:[{ value: "0", output: ""}] })
+  const [score, setScore] = useState(0)
+  const [inGame, setInGame] = useState(false)
+
+  useEffect(() => {
+    generateData()
+    },[]);
+
+  const generateData = () => {
+    let mainGen = Math.floor(Math.random() * 10) + 1
+        let num1Gen = Math.floor(Math.random() * mainGen) + 1
+        let num2Gen = mainGen - num1Gen
+        setAnswers({
+          main: mainGen,
+          num1: num1Gen,
+          num2: num2Gen,
+          choices: [
+            { value: "0", output: num1Gen.toString() + " + " + num2Gen.toString()},
+            { value: "1", output: num1Gen.toString() + " + " + (num2Gen+1).toString()},
+            { value: "2", output: (num1Gen-1).toString() + " + " + num2Gen.toString()},
+            { value: "3", output: (num1Gen+1).toString() + " + " + num2Gen.toString()},
+          ],
+        });
+  }
+
+const shuffle = (array) => {
+  let currentIndex = array.length,  randomIndex;
+  while (currentIndex !== 0) {
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+  console.log(array)
+  return array;
+}
+
+const handleClick = (value) => {
+  if (value === "0") {
+    setScore(score + 100);
+    generateData();
+  } else {
+    setInGame(false); 
+    setScore(0);
+  }
+}
+
+const renderChoice = ({ value, output }) => (
+
+    <button class="button" key={value} onClick={() => handleClick(value)}>{output}</button>
+
+);
+
+  if (inGame) {
+    return (
+      <div className="App">
+        <>
+        <h3>Score: {score}</h3>
+        <h1>{answers.main}</h1>
+        <div className="wrapper">
+          {shuffle(answers.choices).map(renderChoice)}
+        </div>
+        </>
+      </div>
+    );
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <h1>Addition Game</h1>
+        <button onClick={() => setInGame(true)}>Play</button>
+      </>
     </div>
   );
 }
